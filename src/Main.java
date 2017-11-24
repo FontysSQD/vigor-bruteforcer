71650155d928163aab64397a5b13107c60b91db1
Dane Naebers <danenaebers@gmail.com>
2017-11-24 21:39:01 +0100

    idk...

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import javafx.application.Platform;
+
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -11,6 +14,7 @@ import java.util.concurrent.Executors;
 public class Main {
     private static ExecutorService pool;
     private static RequestTask reqTask;
+    private static RequestResult result;
     private static String configFilePath;
     private static String username;
     private static ArrayList<String> passwords;
@@ -44,32 +48,45 @@ public class Main {
     }
 
     private static void bruteForce() throws IOException {
-        for(String s : passwords) {
-            reqTask = new RequestTask(configFilePath) {
+        int listSize = passwords.size() / 8;
+        ArrayList<List<String>> lists = new ArrayList<>();
+        for (int i=0; i<passwords.size(); i += listSize) {
+            lists.add(passwords.subList(i, Math.min(i + listSize, passwords.size())));
+        }
+
+        for(int i = 0; i < 8; i++) {
+            reqTask = new RequestTask(configFilePath, username, lists.get(i)) {
                 @Override
                 protected RequestResult call() throws Exception {
-                    makeRequest(username, s);
+                    makeRequest();
                     return result;
                 }
             };
             pool.submit(reqTask);
         }
+
         pool.execute(new Runnable() {
             @Override
             public void run() {
                 try {
-                    RequestResult result = reqTask.get();
-                    System.out.println(result.password + "     :     " + result.correct);
-                    if(result.correct) {
-                        System.out.println("Password found:  " + result.password);
-                        scanner.nextLine();
-                        System.exit(0);
-                    }
+                    result = reqTask.get();
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println(result.password + "     :     " + result.correct);
+                            if(result.correct) {
+                                System.out.println("Password found:  " + result.password);
+                                scanner.nextLine();
+                                System.exit(0);
+                            }
+                        }
+                    });
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                 }
+
             }
         });
     }
--- src/RequestTask.java
+++ src/RequestTask.java
@@ -1,20 +1,27 @@
 import javafx.concurrent.Task;
 import java.io.IOException;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 public abstract class RequestTask extends Task<RequestResult> implements Observer {
     private RequestClient reqClient;
+    private String username;
+    private List<String> passwordList;
     public RequestResult result;
 
-    public RequestTask(String path) throws IOException {
+    public RequestTask(String path, String username, List<String> passwordList) throws IOException {
+        this.username = username;
+        this.passwordList = passwordList;
         reqClient = new RequestClient(path);
         reqClient.addObserver(this::update);
     }
 
-    public void makeRequest(String username, String password) throws IOException {
+    public void makeRequest() throws IOException {
         synchronized(this) {
-            reqClient.makeRequest(username, password);
+            for(String s : passwordList) {
+                reqClient.makeRequest(username, s);
+            }
         }
     }
 
